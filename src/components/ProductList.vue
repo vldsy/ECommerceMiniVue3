b04d473a9727d139a5adb536f473dd651e1b732b
XoDeR <vldsyata@gmail.com>
2024-10-28 14:41:11 +0100

    product list

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,11 +1,20 @@
 <script setup>
 import ProductCard from './ProductCard.vue';
 import NewProduct from './NewProduct.vue';
+
+import { ref, computed } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const products = computed(() => store.state.products);
+
 </script>
 
 <template>
-    <div>
-        <ProductCard />
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        Product Card
+        <ProductCard v-for="product in products" :key="product.id" :product="product" />
+        New Product
         <NewProduct />
     </div>
 </template>
